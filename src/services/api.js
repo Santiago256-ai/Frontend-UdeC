df8f3ca7accd925c71f3874c5f86f3f6d3073cd7
Santiago256-ai <olivarrodriguez25@gmail.com>
2025-11-16 21:21:35 -0500

    Descripción de los cambios realizados

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backend-udec-production.up.railway.app/api",
+  baseURL: "https://backend-udec-production.up.railway.app",
 });
 
 // Interceptor para agregar token automáticamente
